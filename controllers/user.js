590cbfe2eb987802dc44e4401d0620242d5f3d54
Kwaay <benoit.dum74@gmail.com>
2021-12-12 10:55:16 +0100

    Ajout de l'authentification sur les routes /api/user + partie 'GetAllUsers' pour le controller user.js

--- controllers/user.js
+++ controllers/user.js
@@ -22,64 +22,60 @@ exports.signup = async (req,res,_next) => {
     if (usernameExist) {
         return res.status(409).json({ message: 'Username has already been used'});
     }
-    else if (!regexEmail.test(req.body.email) && (!regexPassword.test(req.body.password))) {
+    if (!regexEmail.test(req.body.email) && (!regexPassword.test(req.body.password))) {
         return res.status(400).json({ message: "Email or Password doesn't have the correct format"});
     }
-    else {
-        try {
-            const hashPassword = await bcrypt.hash(req.body.password, 10)
-            if (req.file) {
+    try {
+        const hashPassword = await bcrypt.hash(req.body.password, 10)
+        if (req.file) {
+            User.create({
+                name: req.body.name,
+                firstname: req.body.firstname,
+                username: req.body.username,
+                email: req.body.email,
+                password: hashPassword,
+                avatar: req.file
+            });
+        }
+        else {
+            const hashEmail = cryptoJS.MD5(req.body.email).toString().toLowerCase();
+            console.log(hashEmail);
+            fetch(`https://www.gravatar.com/avatar/${hashEmail}`, {
+                method: "GET"
+            })
+            .then(function(value) {
+                const gravatarImage = value.url;
+                console.log(gravatarImage);
                 User.create({
                     name: req.body.name,
                     firstname: req.body.firstname,
                     username: req.body.username,
                     email: req.body.email,
                     password: hashPassword,
-                    avatar: req.file
-                });
-            }
-            else {
-                const hashEmail = cryptoJS.MD5(req.body.email).toString().toLowerCase();
-                console.log(hashEmail);
-                fetch(`https://www.gravatar.com/avatar/${hashEmail}`, {
-                    method: "GET"
+                    avatar: gravatarImage
                 })
-                .then(function(value) {
-                    const gravatarImage = value.url;
-                    console.log(gravatarImage);
-                    User.create({
-                        name: req.body.name,
-                        firstname: req.body.firstname,
-                        username: req.body.username,
-                        email: req.body.email,
-                        password: hashPassword,
-                        avatar: gravatarImage
-                    })
-                    .then(done => {
-                        res.status(201).json({ message:'User Created'});
+                .then(done => {
+                    res.status(201).json({ message:'User Created'});
 
-                    })
-                    .catch(error => {
-                        console.log(error);
-                    })
-                    
                 })
-                .catch(error => { 
-                    res.status(500).json({error})
+                .catch(error => {
+                    console.log(error);
                 })
-            }
+                
+            })
+            .catch(error => { 
+                res.status(500).json({error})
+            })
         }
-        catch (error) {
-            res.status(400).json({error});
-        };
+    }
+    catch (error) {
+        res.status(400).json({error});
     };
 }; 
 exports.login = async (req,res,_next) => {
-    console.log(req.body.password);
     if (!regexPassword.test(req.body.password)) {
         return res.status(400).json({ error: "Password doesn't have the correct format" });
     }
-    console.log(req.body)
     // Cas ou l'utilisateur essaye de se connecter avec un username
     if (req.body.hasOwnProperty("username")) {
         if (!regexUsername.test(req.body.username)) {
@@ -146,7 +142,15 @@ exports.login = async (req,res,_next) => {
     }
 };
 exports.getAllUsers = async (req,res,next) => {
-    
+    try {
+        const findAllUsers = await User.findAll()
+        if(findAllUsers) {
+             return res.status(200).json(findAllUsers);
+        }
+    }
+    catch (error) {
+        res.status(400).json({error});
+    }
 }
 exports.createUser = async (req,res,next) => {
     
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
-router.get('/', userCtrl.getAllUsers);
-router.post('/', userCtrl.createUser);
-router.get('/me', userCtrl.myUser);
-router.get('/:userId',userCtrl.getOneUser);
-router.put('/:userId', userCtrl.modifyUser);
-router.delete('/:userId', userCtrl.deleteUser)
+router.get('/', auth, userCtrl.getAllUsers);
+router.post('/', auth, userCtrl.createUser);
+router.get('/me', auth, userCtrl.myUser);
+router.get('/:userId', auth, userCtrl.getOneUser);
+router.put('/:userId', auth, userCtrl.modifyUser);
+router.delete('/:userId', auth, userCtrl.deleteUser)
 
 module.exports = router;
