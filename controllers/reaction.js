94fff354afd423c9255b374442ed25b05ce3244a
Kwaay <benoit.dum74@gmail.com>
2021-12-21 17:56:49 +0100

    Partie 'getAllReactions' & 'createReaction' du controller Reaction

--- controllers/reaction.js
+++ controllers/reaction.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const fsp = require('fs/promises');
 require('dotenv').config()
 
-const { Reaction } = require('../models');
+const { User, Post, Reaction } = require('../models');
 
 exports.getAllReactions = async (req,res,next) => {
     try {
@@ -21,7 +21,32 @@ exports.getAllReactions = async (req,res,next) => {
     }
 }
 exports.createReaction = async (req,res,next) => {
-
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.SECRET_KEY_JWT);
+        const userId = decodedToken.userId
+        console.log(userId)
+        console.log(req.body.postId)
+        const searchReaction = await Reaction.findOne({
+            where: {
+                PostId: req.body.postId, UserId: userId
+            }
+        })
+        if (searchReaction) {
+            return res.status(409).json({message: 'Reaction already exists'})
+        }
+        const reactionCreation = await Reaction.create({
+            type: req.body.type,
+            PostId: req.body.postId,
+            UserId: userId
+        });
+        if (reactionCreation) {
+            return res.status(201).json({ message:'Reaction Created'});
+        }
+    }
+    catch (error) {
+        res.status(400).json({error});
+    }
 }
 exports.getOneReaction = async (req,res,next) => {
 
