1d4986d9a4f192881a6428a2c586adae87bae8bd
Kwaay <benoit.dum74@gmail.com>
2022-02-19 17:11:54 +0100

    📝 Patch doc API

--- controllers/user.js
+++ controllers/user.js
@@ -344,9 +344,9 @@ exports.forgot = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
-    return res.status(200).json(
-      user.question,
-    );
+    return res.status(200).json({
+      reponse: user.question,
+    });
   } catch (error) {
     return res.status(500).json({ message: 'Something went wrong. Please try again' });
   }
